8c8d0b8ca2616e9f14108db934186090da8929ae
한지원 <hanjiwon@hanjiwon-ui-MacBookAir.local>
2023-10-25 21:47:13 +0900

    baekjoon

---
+++ baekjoon_8393/baekjoon_8393.js
@@ -0,0 +1,11 @@
+let input = require('fs').readFileSync('input.txt').toString().split(' ');
+
+const N = Number(input.shift());
+
+let sum = 0;
+
+for (let A = 1; A <= N; A++) {
+    sum = A + sum;
+}
+
+console.log(sum);
